4ff830811a56272b70c75550e12b907e3c73a88c
Lasarussp <001lasarus@gmail.com>
2022-12-23 12:14:32 +0530

    err solved

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const connect = require('./db/connect_db');
 const studentRoute = require('./routes/student_route');
 const mentorRoute = require('./routes/mentor_route');
+const { default: mongoose } = require('mongoose');
 
 // dotenv environment setup
 require('dotenv').config();
+
+// deprecation err
+mongoose.set('strictQuery', true);
 
 // web server
 const app = express();
@@ -14,7 +18,7 @@ app.use(express.json());
 app.use('/', studentRoute);
 app.use('/', mentorRoute);
 
-let port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, async () => {
   console.log(`The App is running on the port ${port}`);
